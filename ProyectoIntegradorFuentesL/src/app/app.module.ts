aee5f7da19146724e9989c0d945554c04a715596
lucho23fuentes88 <69926848+lucho23fuentes88@users.noreply.github.com>
2022-03-15 13:49:43 -0300

    se elimino node module

--- ProyectoIntegradorFuentesL/src/app/app.module.ts
+++ ProyectoIntegradorFuentesL/src/app/app.module.ts
@@ -9,8 +9,9 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
-import { NgCircleProgressModule } from 'ng-circle-progress';
+
 import { HysComponent } from './components/hys/hys.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
