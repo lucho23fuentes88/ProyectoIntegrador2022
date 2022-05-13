2a271cec0bc96ec44f1e18b6c21fb0ece9f7b5f4
lucho23fuentes88 <69926848+lucho23fuentes88@users.noreply.github.com>
2022-05-13 13:14:56 -0300

    .

---
+++ ProyectoIntegradorFuentesL/BackEnd/src/main/java/com/portfolio/lgb/LgbApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.lgb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LgbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LgbApplication.class, args);
+	}
+
+}
