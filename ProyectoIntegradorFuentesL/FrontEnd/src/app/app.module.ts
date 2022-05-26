42b62f6ffa8308abf76dfb88a79d08121a5c52db
lucho23fuentes88 <69926848+lucho23fuentes88@users.noreply.github.com>
2022-05-26 09:59:55 -0300

    import

--- ProyectoIntegradorFuentesL/FrontEnd/src/app/app.module.ts
+++ ProyectoIntegradorFuentesL/FrontEnd/src/app/app.module.ts
@@ -8,11 +8,11 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
-
 import { HysComponent } from './components/hys/hys.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
