a7e8bb45a0333403fc021a77e2cc7f02450e2cb9
lucho23fuentes88 <69926848+lucho23fuentes88@users.noreply.github.com>
2022-07-14 12:29:16 -0300

    persona service

--- ProyectoIntegradorFuentesL/FrontEnd/src/app/components/acerca-de/acerca-de.component.ts
+++ ProyectoIntegradorFuentesL/FrontEnd/src/app/components/acerca-de/acerca-de.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { persona } from 'src/app/model/persona.model';
+import { PersonaService } from 'src/app/service/persona.service';
 
 @Component({
   selector: 'app-acerca-de',
@@ -8,7 +9,8 @@ import { persona } from 'src/app/model/persona.model';
 })
 export class AcercaDeComponent implements OnInit {
   persona: persona = new persona("","","");
-  constructor() { }
+
+  constructor(public personaService: PersonaService) { }
 
   ngOnInit(): void {
   }
